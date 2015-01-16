e1671e4840649199d9d8cffa2b719b865cf71659
Cristina Ponce Torres <cristinaponce.designer@gmail.com>
2015-01-16 23:06:37 +0100

    First upload
    Install and change headers.

---
+++ wp-content/themes/twentyfifteen/header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<script>(function(){document.documentElement.className='js'})();</script>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
+
+	<div id="sidebar" class="sidebar">
+		<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding">
+				<img src="<?php bloginfo('url');?>/wp-content/uploads/2015/01/logotype.png" />
+				<button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
+			</div><!-- .site-branding -->
+		</header><!-- .site-header -->
+	</div><!-- .sidebar -->
+	<div class="sidebar light_blue">
+		<h1>Gracias por realizar nuestra prueba</h1>
+	</div>
+
+	<div id="content" class="site-content">
