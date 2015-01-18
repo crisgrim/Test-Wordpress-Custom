1d449af66e373c3c36e7e6634745c8b90c8fad02
Cristina Ponce Torres <cristinaponce.designer@gmail.com>
2015-01-18 11:07:45 +0100

    Editing Homepage
    Remove Title, link pages, comments, sidebar.
    Added Flexslider with php get code and ‘productos descatados’.

--- wp-content/themes/twentyfifteen/homepage.php
+++ wp-content/themes/twentyfifteen/homepage.php
@@ -34,24 +34,99 @@ get_header(); ?>
 						</div>
 						<?php endif; ?>
 
-						<h1 class="entry-title"><?php the_title(); ?></h1>
 					</header><!-- .entry-header -->
+
+					<div class="flexslider">
+					    <ul id="mySlider" class="slides">
+					    <?php
+					    $rows = get_field('slide');
+					    if($rows) {
+					 
+					        foreach($rows as $row) {
+					            // retrieve size 'large' for background image
+					            $bgimg = $row['bg_image']['sizes']['large'];
+					 
+					            $output = "<li class='oneSlide'>\n";
+					            $output .= "  <div class='slide-text'>\n";
+					            $output .= "  <img src='".$bgimg."' />\n";
+					            $output .= "  <div class='block-text'>\n";
+					            $output .= "  <h2>". $row['slide_heading'] ."</h2>\n";
+					            $output .= "  " . $row['slide_text'];
+					            $output .= "  </div>\n";
+					            $output .= "  </div>\n";
+					            $output .= "</li>\r\n\n";
+					 
+					            echo $output;
+					        }
+					    }
+					 
+					    ?>
+					    </ul>
+					</div>
 
 					<div class="entry-content">
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 					</div><!-- .entry-content -->
 
 					<footer class="entry-meta">
 						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
 					</footer><!-- .entry-meta -->
 				</article><!-- #post -->
-
-				<?php comments_template(); ?>
 			<?php endwhile; ?>
+
+			<div class="productos-destacados">
+			<!--
+				<?php
+					$terms = get_the_terms( $post->ID, 'productos_home' );
+
+					if ( $terms && ! is_wp_error( $terms ) ) :
+
+					   $tags = array();
+
+					   foreach ( $terms as $term ) {
+					      $tags[] = $term->name;
+					   }
+
+					   $tags = join( ", ", $tags );
+
+					   ?>
+				   <p>Actors: <span><?php echo $tags; ?></span></p>
+				<?php endif; ?>
+			-->
+			<!--
+				<?php
+					$args = array(
+						'post_type' => 'productos_destacados',
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'productos_home'
+							),
+						),
+					);
+					$query = new WP_Query( $args );
+					while ($query->have_posts()) : $query->the_post();
+				?>
+				<?php the_content( $more_link_text, $stripteaser ); ?>
+				<?php endwhile; ?>
+			-->
+			<?php
+				$args = array( 'post_type' => 'productos_destacados', 'posts_per_page' => 3 );
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post();
+				
+				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+					the_post_thumbnail();
+				}
+				  the_title();
+				  echo '<div class="entry-content">';
+				  the_content();
+				  echo '</div>';
+				endwhile;
+			?>
+			</div>
+
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
